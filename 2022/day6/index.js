277cc5722101c63245f9ce362b0e411983776c82
Daniele Scotti <daniele@dueper.net>
2022-12-06 22:55:12 +0100

    refactoring day 6

--- 2022/day6/index.js
+++ 2022/day6/index.js
@@ -3,44 +3,27 @@ const _ = require('lodash');
 
 const data = fs.readFileSync(`data.txt`).toString().split("");
 
-const markerLenght = 4
-let test = []
-let index = 0
+const findFirstMarker = (markerLength) => {
+  let test = []
+  let index = 0
 
-const firstMarker = data.findIndex((letter, i) => {
-  const position = test.indexOf(letter)
+  const firstMarker = data.findIndex((letter, i) => {
+    const position = test.indexOf(letter)
 
-  if (position === -1) {
-    index++
-  } else {
-    test.splice(0, position + 1)
-    index = index - position
-  }
+    if (position === -1) {
+      index++
+    } else {
+      test.splice(0, position + 1)
+      index = index - position
+    }
 
-  test.push(letter)
+    test.push(letter)
 
-  if (index === markerLenght) return true
-})
+    if (index === markerLength) return true
+  })
 
-console.log("Part 1:", firstMarker + 1)
+  return firstMarker + 1
+}
 
-const messageLenght = 14
-let messageTest = []
-let messageIndex = 0
-
-const firstMessages = data.findIndex((letter, i) => {
-  const position = messageTest.indexOf(letter)
-
-  if (position === -1) {
-    messageIndex++
-  } else {
-    messageTest.splice(0, position + 1)
-    messageIndex = messageIndex - position
-  }
-
-  messageTest.push(letter)
-
-  if (messageIndex === messageLenght) return true
-})
-
-console.log("Part 2:", firstMessages + 1)
+console.log("Part 1:", findFirstMarker(4))
+console.log("Part 2:", findFirstMarker(14))
